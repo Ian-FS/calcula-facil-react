29cd5f59f7624a8ad4ff3ae43c8be1083ec5255c
Ian_França <iiann_2009@hotmail.com>
2023-05-23 22:34:13 -0300

    Atualizado logica da página End Pipe

---
+++ src/components/Formulario/ResultDashboard/ResultDashboard.jsx
@@ -0,0 +1,7 @@
+export default function ResultDashboard() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/pages/calculates-end-pipe.jsx
+++ src/pages/calculates-end-pipe.jsx
@@ -2,11 +2,12 @@ import InputBox from "../components/Formulario/InputBox/InputBox"
 import InputCheck from "../components/Formulario/InputCheck/InputCheck"
 import Botao from "../components/Formulario/Botao/Botao"
 import { useState } from "react"
+import ResultDashboard from "../components/Formulario/ResultDashboard/ResultDashboard";
 
 export default function EndPipePage() {
-    const [carcassTotal, setCarcassTotal] = useState();
-    const [partialExtrusion, setPartialExtrusion] = useState();
-    const [speedLine, setSpeedLine] = useState();
+    const [carcassTotal, setCarcassTotal] = useState('');
+    const [partialExtrusion, setPartialExtrusion] = useState('');
+    const [speedLine, setSpeedLine] = useState('');
 
     const diasDaSemana = [
         'domingo',
@@ -18,38 +19,49 @@ export default function EndPipePage() {
         'sábado'
     ]
 
-    const CalculaTermino = () => {
-        const data = new Date()
-        const diaDaSemana = diasDaSemana[data.getDay()]
-        const dia = data.getDate()
-        const mes = data.getMonth() + 1
-        const ano = data.getFullYear()
-        const hora = data.getHours()
-        const minutos = data.getMinutes()
+    const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-        const diaConvertido = Math.trunc((1970 / 60) / 24) // dias
-        const horaConvertida = Math.trunc(1970 / 60) // horas
-        const minutoConvertida = 1970 % 60 //minutos
+    const calculaTermino = () => (carcassTotal - partialExtrusion) / speedLine
+    const calculaTempoTermino = () => {
 
-        console.log(diaConvertido, horaConvertida, minutoConvertida)
+        let currentDate = new Date()
+        let newDate = new Date()
+
+        const diasConvertidos = Math.trunc((calculaTermino() / 60) / 24) // dias
+        const horasConvertidas = Math.trunc(calculaTermino() / 60) // horas
+        const minutosConvertidos = calculaTermino() % 60 //minutos
+
+
+        newDate.setDate(currentDate.getDate() + diasConvertidos)
+        newDate.setHours(currentDate.getHours() + horasConvertidas)
+        newDate.setMinutes(currentDate.getMinutes() + minutosConvertidos)
+
+        let diaDaSemana = diasDaSemana[newDate.getDay()]
+
+        console.log(`O tubo terminará na ${diaDaSemana}, ${newDate.getDate()} de ${meses[newDate.getMonth()]} de ${newDate.getFullYear()}, às ${newDate.getHours()}:${newDate.getMinutes()}.`)
+        return <p>
+            {`O tubo terminará na ${diaDaSemana}, ${newDate.getDate()} de ${meses[newDate.getMonth()]} de ${newDate.getFullYear()}, às ${newDate.getHours()}:${newDate.getMinutes()}.`}
+        </p>
     }
+
+
 
     return (
         <div className="form">
             <InputBox
-                handleChange={(event) => setCarcassTotal(event.target.value)}
+                handleChange={(event) => setCarcassTotal(parseFloat(event.target.value))}
                 labelBox={'Carcaça total'}
                 placeholder={'Informe a metragem'}
                 value={carcassTotal}
             />
             <InputBox
-                handleChange={(event) => setPartialExtrusion(event.target.value)}
+                handleChange={(event) => setPartialExtrusion(parseFloat(event.target.value))}
                 labelBox={'Extrusão parcial'}
                 placeholder={'Informe a metragem'}
                 value={partialExtrusion}
             />
             <InputBox
-                handleChange={(event) => setSpeedLine(event.target.value)}
+                handleChange={(event) => setSpeedLine(parseFloat(event.target.value))}
                 labelBox={'Velocidade da Linha'}
                 placeholder={'Informe a velocidade'}
                 value={speedLine}
@@ -70,9 +82,10 @@ export default function EndPipePage() {
                 <InputCheck labelCheck={'Ferramenta'} />
             </div>
             <Botao
-                handleClick={CalculaTermino}
+                handleClick={calculaTempoTermino}
             />
+            {calculaTempoTermino()}
+            <ResultDashboard />
         </div>
-
     )
 }
