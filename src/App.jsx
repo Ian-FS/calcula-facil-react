5e05ad647fb1501167531bb1408d09465922c168
Ian_França <iiann_2009@hotmail.com>
2023-05-26 10:42:19 -0300

    atualizado o caminho das rotas do componente app

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,14 @@ import { useState } from 'react';
 function App() {
 
   const [showResult, setShowResult] = useState(false)
-  // const abreContainer = () => {
-  //   document.getElementById('container').style.transition = '1s'
-  //   document.getElementById('container').style.transform = 'scale(1)';
-  // }
   return (
     <div>
       {!showResult && <Header ></Header>}
       <div id={'container'} className='container'>
         <Routes>
           <Route index element={<HomePage />} />
-          <Route path='terminopipe' element={<EndPipePage showResult={showResult} setShowResult={setShowResult} />} />
-          <Route path='compressao' element={<CompressionPage />} />
+          <Route path='/terminopipe' element={<EndPipePage showResult={showResult} setShowResult={setShowResult} />} />
+          <Route path='/compressao' element={<CompressionPage />} />
           <Route path='*' element={<h1>Página não encontrada</h1>} />
         </Routes>
       </div>
