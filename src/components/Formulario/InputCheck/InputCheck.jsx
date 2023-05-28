4a8409e9d7e3f06eccf323183e730c5926a5ce7c
Ian_França <iiann_2009@hotmail.com>
2023-05-28 17:22:15 -0300

    atualizado logica do componente inputcheck e da pagina endPipe

--- src/components/Formulario/InputCheck/InputCheck.jsx
+++ src/components/Formulario/InputCheck/InputCheck.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import './InputCheck.scss'
 
-export default function InputCheck({ labelCheck, nameCheck, valueCheck }) {
+export default function InputCheck({ labelCheck, nameCheck, valueCheck, setCheck }) {
     return (
         <div className="form-control check">
             <input
                 name={nameCheck}
                 type="radio"
                 value={valueCheck}
+                onClick={(event) => {
+                    setCheck(parseFloat(event.target.value))
+                }}
             />
             <label
                 htmlFor="line"
--- src/pages/calculates-end-pipe.jsx
+++ src/pages/calculates-end-pipe.jsx
@@ -10,6 +10,10 @@ export default function EndPipePage({ showResult, setShowResult }) {
     const [produced, setProduced] = useState('');
     const [speedLine, setSpeedLine] = useState('');
     const [activeStyle, setActiveStyle] = useState(false)
+    const [valueRadioLine, setValueRadioLine] = useState(0)
+    // const [valueRadioRef, setValueRadioRef] = useState(0)
+
+    console.log(typeof valueRadioLine)
 
     const diasDaSemana = [
         'domingo',
@@ -35,21 +39,32 @@ export default function EndPipePage({ showResult, setShowResult }) {
         'Dezembro'
     ]
 
-    const endTime = () => (toBeProduced - produced) / speedLine
+    const endTime = () => (toBeProduced - produced - valueRadioLine / speedLine)
+
+    console.log(endTime())
 
     const convertedTimes = {
         days: Math.trunc((endTime() / 60) / 24), // dias
         hours: Math.trunc(endTime() / 60), // horas
         minutes: (endTime() % 60).toFixed(0) //minutos
     }
+    console.log(convertedTimes.days)
+    console.log(convertedTimes.hours)
+    console.log(convertedTimes.minutes)
 
     const addTime = () => {
         let currentDate = new Date()
         let newDate = new Date()
 
+        console.log(currentDate)
         newDate.setDate(currentDate.getDate() + convertedTimes.days)
+        console.log(newDate)
         newDate.setHours(currentDate.getHours() + convertedTimes.hours)
+        console.log(newDate)
         newDate.setMinutes(currentDate.getMinutes() + (convertedTimes.minutes))
+        console.log(newDate)
+
+
 
         const diaDaSemana = diasDaSemana[newDate.getDay()]
         const newMinutes = newDate.getMinutes()
@@ -58,7 +73,9 @@ export default function EndPipePage({ showResult, setShowResult }) {
         const newMonth = newDate.getMonth()
         const newYears = newDate.getFullYear()
 
-        let mensagem = `O tubo terminará no(a) ${diaDaSemana}, ${(newDays.toString()).padStart(2, '0')} de ${meses[newMonth]} de ${newYears}, às ${newHours}:${(newMinutes.toString()).padStart(2, '0')}.`
+        console.log(newDays, newMonth, newYears, ' ', newHours, newMinutes)
+
+        let mensagem = `O tubo terminará no(a) ${diaDaSemana}, ${(newDays.toString()).padStart(2, '0')} de ${meses[newMonth]} de ${newYears}, às ${(newHours.toString()).padStart(2, '0')}:${(newMinutes.toString()).padStart(2, '0')}.`
 
         return mensagem
     }
@@ -85,7 +102,6 @@ export default function EndPipePage({ showResult, setShowResult }) {
     const handleClick = (event) => {
         event.preventDefault();
         checkInput()
-
     }
 
     return (
@@ -120,33 +136,38 @@ export default function EndPipePage({ showResult, setShowResult }) {
                             setActiveStyle={setActiveStyle}
                             activeStyle={activeStyle}
                         />
+
+                        {/* <div id={'end-refer'} className="end-refer">
+                            <InputCheck
+                                labelCheck={'Underroller'}
+                                nameCheck={'refer'}
+                                valueCheck={104}
+                            />
+                            <InputCheck
+                                labelCheck={'Ferramenta'}
+                                nameCheck={'refer'}
+                            />
+                        </div> */}
+
                         <div id={'number-line'} className="number-line">
                             <InputCheck
                                 labelCheck={'Linha 1'}
                                 nameCheck={'line'}
-                                checked={true}
                                 valueCheck={58}
+                                setCheck={setValueRadioLine}
                             />
                             <InputCheck
                                 labelCheck={'Linha 2'}
                                 nameCheck={'line'}
-                                checked={false}
                                 valueCheck={56}
+                                setCheck={setValueRadioLine}
                             />
                             <InputCheck
                                 labelCheck={'Linha 3'}
                                 nameCheck={'line'}
                                 valueCheck={61}
+                                setCheck={setValueRadioLine}
                             />
-                        </div>
-                        <div id={'end-refer'} className="end-refer">
-                            <InputCheck
-                                labelCheck={'Underroller'}
-                                nameCheck={'refer'}
-                            />
-                            <InputCheck
-                                labelCheck={'Ferramenta'}
-                                nameCheck={'refer'} />
                         </div>
                         <Botao />
                     </div>
