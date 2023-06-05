a18978cdf8eb3f1b50b0ce66ad2d5f5010502d43
Ian_França <iiann_2009@hotmail.com>
2023-06-04 21:44:57 -0300

    atualizado lógica da página calculates end pipe

--- src/pages/calculates-end-pipe.jsx
+++ src/pages/calculates-end-pipe.jsx
@@ -38,30 +38,12 @@ export default function EndPipePage({ showResult, setShowResult }) {
     ]
 
     const endTime = () => ((toBeProduced - produced - valueRadioLine) / speedLine)
+    console.log(endTime())
 
-    // console.log(`minutos calculados: ${endTime()}`)
-
-    const convertedTimes = {
-        days: Math.trunc((endTime() / 60) / 24), // dias
-        hours: Math.trunc(((endTime() / 1440) - Math.trunc(endTime() / 1440)) * 60) - (24 * Math.trunc(endTime() / 1440)), // horas
-        minutes: parseFloat((endTime() % 60).toFixed(0)) //minutos
-    }
-    console.log(`${endTime()}: ${convertedTimes.days} dias, ${convertedTimes.hours} horas e ${convertedTimes.minutes} minutos.`)
-    console.log(3000 % 7)
     const addTime = () => {
 
-        let currentDate = new Date()
-        console.log(`data atual:${currentDate}`)
         let newDate = new Date()
-
-        newDate.setDate(currentDate.getDate() + convertedTimes.days)
-        // console.log(`apos adicionar os dias: ${newDate.getDate()}`)
-        newDate.setHours(currentDate.getHours() + convertedTimes.hours)
-        // console.log(`apos adicionar as horas: ${newDate.getHours()}`)
-        newDate.setMinutes(currentDate.getMinutes() + convertedTimes.minutes)
-        // console.log(`apos adicionar os minutos: ${newDate.getMinutes()}`)
-
-
+        newDate.setMinutes(newDate.getMinutes() + endTime())
 
         const diaDaSemana = diasDaSemana[newDate.getDay()]
         const newMinutes = newDate.getMinutes()
@@ -70,10 +52,8 @@ export default function EndPipePage({ showResult, setShowResult }) {
         const newMonth = newDate.getMonth()
         const newYears = newDate.getFullYear()
 
-        console.log(newDate.getUTCDate())
         if (endTime() > 0 && endTime() < 999999) {
             let mensagem = `Término do tubo na boca da ferramenta previsto para ${diaDaSemana}, ${(newDays.toString()).padStart(2, '0')} de ${meses[newMonth]} de ${newYears}, às ${(newHours.toString()).padStart(2, '0')}:${(newMinutes.toString()).padStart(2, '0')}.`
-
             return mensagem
         } else {
             let mensagem = 'Revise os valores atribuidos!'
@@ -83,7 +63,6 @@ export default function EndPipePage({ showResult, setShowResult }) {
 
     const checkInput = () => {
         if (toBeProduced === "") {
-            // console.log(toBeProduced)
             setActiveStyle(true)
             return false
         }
@@ -104,7 +83,6 @@ export default function EndPipePage({ showResult, setShowResult }) {
             return true
         }
     }
-
 
     const handleClick = (event) => {
         event.preventDefault();
@@ -147,7 +125,7 @@ export default function EndPipePage({ showResult, setShowResult }) {
                             <InputCheck
                                 labelCheck={'Linha 1'}
                                 nameCheck={'line'}
-                                valueCheck={58}
+                                valueRadio={58}
                                 setValueRadio={setValueRadioLine}
                                 setCheckLine={setCheckLine}
                                 check={check}
@@ -157,7 +135,7 @@ export default function EndPipePage({ showResult, setShowResult }) {
                             <InputCheck
                                 labelCheck={'Linha 2'}
                                 nameCheck={'line'}
-                                valueCheck={56}
+                                valueRadio={56}
                                 setValueRadio={setValueRadioLine}
                                 setCheckLine={setCheckLine}
                                 check={check}
@@ -167,7 +145,7 @@ export default function EndPipePage({ showResult, setShowResult }) {
                             <InputCheck
                                 labelCheck={'Linha 3'}
                                 nameCheck={'line'}
-                                valueCheck={61}
+                                valueRadio={61}
                                 setValueRadio={setValueRadioLine}
                                 setCheckLine={setCheckLine}
                                 check={check}
